d82ffaec527a963ec450959ed045a7a649bd7b91
eyerean <eptinea@gmail.com>
2021-08-15 16:22:36 +0100

    connect to mongo database

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// connect to the db
+mongoose.connect(
+  process.env.DB_CONNECTION_URI,
+  { useNewUrlParser: true },
+  () => {
+    console.log('Connected to the db!');
+  }
+);
 
 // API routes
 // anything beginning with "/api" will go into this
