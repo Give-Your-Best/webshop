460c12b8e732a6779af255f3d9b1922b7fda20ac
eyerean <eptinea@gmail.com>
2021-10-28 21:53:13 +0100

    move import

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const bcrypt = require('bcryptjs');
+
+const Schema = mongoose.Schema;
 
 // define the mongoose user model
 const userSchema = new Schema({
