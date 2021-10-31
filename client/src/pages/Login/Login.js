39229cc51a3b76bf5ac59b1e200b013149ea58ba
eyerean <eptinea@gmail.com>
2021-10-31 16:49:52 +0000

    call login post endpoint, handle error

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,13 +1,50 @@
 import * as React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const loginUser = async ({ username, password }) => {
+  try {
+    const response = await fetch('/api/auth/login', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+    const jsonres = await response.json();
+    return jsonres;
+  } catch (error) {
+    console.error(`Error in loginUser: ${error}`);
+    return error;
+  }
+};
 
 export const Login = () => {
+  let history = useHistory();
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [errorMessage, setErrorMessage] = React.useState('');
 
-  const handleLoginSubmit = (event) => {
+  React.useEffect(() => {
+    if (username || password) {
+      setErrorMessage('');
+    }
+  }, [username, password]);
+
+  const handleLoginSubmit = async (event) => {
     event.preventDefault();
 
-    console.log('data!', username, password);
+    const res = await loginUser({ username, password });
+    console.log('res!', res);
+    if (res.success) {
+      // set global state with user obj + token (?)
+      history.push('/');
+    } else {
+      setErrorMessage(res.message);
+    }
   };
 
   return (
@@ -36,6 +73,7 @@ export const Login = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
         <button>login</button>
       </form>
     </div>
