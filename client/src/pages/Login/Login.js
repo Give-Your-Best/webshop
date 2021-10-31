f512cf7b258a97c034e0e4881ec9bedb224d2e41
eyerean <eptinea@gmail.com>
2021-10-31 16:14:05 +0000

    add basic login form

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,9 +1,43 @@
 import * as React from 'react';
 
 export const Login = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('data!', username, password);
+  };
+
   return (
     <div>
-      <h1>{`Login page!`}</h1>
+      <h2>Login</h2>
+      <form onSubmit={handleLoginSubmit}>
+        <div>
+          <label htmlFor="username" style={{ marginRight: 16 }}>
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password" style={{ marginRight: 16 }}>
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button>login</button>
+      </form>
     </div>
   );
 };
