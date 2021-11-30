b87324de4302924bc19b5ad32ab5b4302b998d77
eyerean <eptinea@gmail.com>
2021-11-30 19:23:41 +0000

    add underline style to link

--- client/src/components/atoms/Link/Link.js
+++ client/src/components/atoms/Link/Link.js
@@ -2,4 +2,8 @@ import styled from 'styled-components';
 
 export const Link = styled.a`
   color: ${({ theme }) => theme.colorMappings.primary};
+
+  :hover {
+    text-decoration: underline;
+  }
 `;
