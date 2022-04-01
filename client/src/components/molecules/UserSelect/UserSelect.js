c0225546a7b8f1f7a522d912d9227e3b91c7317f
Zahra-8d <me@zahraweb.uk>
2022-04-01 18:16:50 +0100

    wee fix

--- client/src/components/molecules/UserSelect/UserSelect.js
+++ client/src/components/molecules/UserSelect/UserSelect.js
@@ -5,7 +5,7 @@ export const UserSelect = ({ users, selectName, fieldPlaceholder, disabled }) =>
   return (
     <StyledSelect name={`${selectName}`} placeholder={fieldPlaceholder || 'Select an option'} disabled={disabled}>
         {users.map((d)=>{
-            return (<StyledSelect.Option key={d._id} value={d._id}>{d.name}</StyledSelect.Option>);
+            return (<StyledSelect.Option key={d._id} value={d._id}>{d.name || (d.firstName + ' ' + d.lastName)}</StyledSelect.Option>);
         })}
     </StyledSelect>
   );
