7503e38d4d10b0a1f879d3800103d98f1b7178d8
Zahra-8d <me@zahraweb.uk>
2022-06-17 18:02:50 +0100

    bug fixes

--- server/utils/mailjet.js
+++ server/utils/mailjet.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const Mailjet = require('node-mailjet');
 
 const mailjet = new Mailjet({
-    apiKey: '39dc5a3675516d08023d9bbf5f642ebc',
-    apiSecret: 'e614e7b1ad85e9b7e4fb432a6d2075f4'
+    apiKey: process.env.MAILJET_KEY,
+    apiSecret: process.env.MAILJET_SECRET
 });
 
 module.exports = { mailjet };
