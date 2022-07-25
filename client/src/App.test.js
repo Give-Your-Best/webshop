c5895f18b617e4725b250d791163c14600a10385
Zahra-8d <me@zahraweb.uk>
2022-07-25 17:13:34 +0100

    test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,9 +2,15 @@ import { render, screen } from './config/test-utils';
 import App from './App';
 
 describe('<App />', () => {
-//   describe('when on / route', () => {
-//     it('renders the app header', () => {
-//       expect(screen.getByTestId('Header')).toBeInTheDocument();
-//     });
-//   });
+  describe('when on / route', () => {
+    // beforeEach(() => {
+    //   render(<App />);
+    // });
+    // it('renders the home page', () => {
+    //   expect(screen.getByTestId('HomeRoute')).toBeInTheDocument();
+    // });
+    // it('renders the app header', () => {
+    //   expect(screen.getByTestId('Header')).toBeInTheDocument();
+    // });
+  });
 });
