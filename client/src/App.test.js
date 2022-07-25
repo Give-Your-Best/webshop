8b3a7106626ab99ae80699feed2fe5e1a9314587
Zahra-8d <me@zahraweb.uk>
2022-07-25 16:13:33 +0100

    donor approval, image front bug, shopper count

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from './config/test-utils';
+import App from './App';
+
+describe('<App />', () => {
+  describe('when on / route', () => {
+    it('renders the app header', () => {
+      expect(screen.getByTestId('Header')).toBeInTheDocument();
+    });
+  });
+});
