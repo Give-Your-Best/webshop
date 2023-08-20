6aed79006a4a082df976cd5e2b5459d92242be12
Haydn Appleby <haydnba@pm.me>
2023-08-20 23:48:56 +0100

    adds get and upsert message static handlers on the messages schema definition

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const User_ = require('./User');
+const BSON = require('bson');
 const options = { timestamps: true };
 
 const messageSchema = new Schema(
@@ -12,18 +12,18 @@ const messageSchema = new Schema(
     threadId: String,
     subject: String,
     user: {
-      type: mongoose.Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: 'User',
     },
     messages: [
       {
         message: String,
         recipient: {
-          type: mongoose.Schema.Types.ObjectId,
+          type: Schema.Types.ObjectId,
           ref: 'User',
         },
         sender: {
-          type: mongoose.Schema.Types.ObjectId,
+          type: Schema.Types.ObjectId,
           ref: 'User',
         },
         viewed: {
@@ -36,6 +36,55 @@ const messageSchema = new Schema(
   },
   options
 );
+
+// TODO
+messageSchema.statics.getAllOfType = async function (type) {
+  const threads = await this.find({ type })
+    .populate('user')
+    .populate('messages.sender')
+    .populate('messages.recipient')
+    .sort({ updatedAt: -1 });
+
+  return threads;
+};
+
+// TODO
+messageSchema.statics.getAllForUser = async function (user) {
+  const threads = await this.find({ user })
+    .populate('user')
+    .populate('messages.sender')
+    .populate('messages.recipient')
+    .sort({ updatedAt: -1 });
+
+  return threads;
+};
+
+// TODO
+messageSchema.statics.upsertThread = async function (data) {
+  const threadId = data.threadId || new BSON.ObjectId();
+
+  const { sender, recipient, sentDate, message, viewed, ...rest } = data;
+
+  const messages = {
+    message,
+    recipient,
+    sender,
+    sentDate,
+    viewed: false,
+  };
+
+  const thread = await this.findOneAndUpdate(
+    { threadId },
+    { ...rest, $push: { messages } },
+    { new: true, upsert: true, useFindAndModify: false }
+  );
+
+  return thread
+    .populate('user')
+    .populate('messages.sender')
+    .populate('messages.recipient')
+    .execPopulate();
+};
 
 //export
 module.exports = mongoose.model('Message', messageSchema);
