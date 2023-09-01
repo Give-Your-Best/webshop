ad8e7f4b3642a17268c3239ebe527d8b0f42f1ab
Haydn Appleby <haydnba@pm.me>
2023-09-01 11:49:25 +0100

    provide the socket context across the whole dashboard

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -2,13 +2,16 @@ import * as React from 'react';
 import { useParams } from 'react-router-dom';
 import { Container } from '../../components';
 import { DashboardTabs } from '../../components/';
+import { SocketProvider } from '../../context/socket-context';
 
 export const Dashboard = () => {
   const { itemId } = useParams();
 
   return (
     <Container data-testid="Dashboard">
-      <DashboardTabs itemId={itemId} />
+      <SocketProvider>
+        <DashboardTabs itemId={itemId} />
+      </SocketProvider>
     </Container>
   );
 };
