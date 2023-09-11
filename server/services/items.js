dae12b440b0f07517013ac927c7c382c2d363733
Haydn Appleby <haydnba@pm.me>
2023-09-12 00:07:35 +0100

    fix filename omission

--- server/services/items.js
+++ server/services/items.js
@@ -51,83 +51,83 @@ const createItem = async (data) => {
 };
 //messages text and timestamp in the messages table . Use sendgrid.
 const updateItem = async (id, updateData) => {
-  var results = {};
-  var new_photos = [];
-  if (updateData.photos) {
-    //if there are images to add to the item then upload to cloudianary and add cloudinary links to the update data
-    const promises = updateData.photos.map(async (photo) => {
-      if (photo.status == 'removed') {
-        try {
-          return cloudinary.uploader.destroy(photo.publicId);
-        } catch (err) {
-          return {};
-        }
-      } else if (!photo.url && photo.imageUrl) {
-        return cloudinary.uploader
-          .upload(photo.imageUrl, {
-            resource_type: 'auto',
-            public_id: photo.uid,
-            overwrite: false,
-            secure: true,
-          })
-          .then((result) => {
-            console.log('*** Success: Cloudinary Upload: ', result.secure_url);
-            new_photos.push({
-              url: result.secure_url,
-              name: photo.name,
-              createdAt: result.created_at,
-              publicId: photo.uid,
-              front: photo.front ? true : false,
-            });
-          })
-          .catch((err) => {
-            console.error(err);
-            console.log('*** Error: Cloudinary Upload');
-          });
-      } else if (Object.keys(photo).length) {
-        new_photos.push(photo);
-        return true;
-      }
+  // var results = {};
+  // var new_photos = [];
+  // if (updateData.photos) {
+  //   //if there are images to add to the item then upload to cloudianary and add cloudinary links to the update data
+  //   const promises = updateData.photos.map(async (photo) => {
+  //     if (photo.status == 'removed') {
+  //       try {
+  //         return cloudinary.uploader.destroy(photo.publicId);
+  //       } catch (err) {
+  //         return {};
+  //       }
+  //     } else if (!photo.url && photo.imageUrl) {
+  //       return cloudinary.uploader
+  //         .upload(photo.imageUrl, {
+  //           resource_type: 'auto',
+  //           public_id: photo.uid,
+  //           overwrite: false,
+  //           secure: true,
+  //         })
+  //         .then((result) => {
+  //           console.log('*** Success: Cloudinary Upload: ', result.secure_url);
+  //           new_photos.push({
+  //             url: result.secure_url,
+  //             name: photo.name,
+  //             createdAt: result.created_at,
+  //             publicId: photo.uid,
+  //             front: photo.front ? true : false,
+  //           });
+  //         })
+  //         .catch((err) => {
+  //           console.error(err);
+  //           console.log('*** Error: Cloudinary Upload');
+  //         });
+  //     } else if (Object.keys(photo).length) {
+  //       new_photos.push(photo);
+  //       return true;
+  //     }
+  //   });
+  //   await Promise.all(promises);
+  //   if (new_photos.length) {
+  //     updateData.photos = new_photos;
+  //   } else {
+  //     delete updateData.photos;
+  //   }
+  try {
+    const item = await Item.findOneAndUpdate({ _id: id }, updateData, {
+      useFindAndModify: false,
+      returnDocument: 'after',
     });
-    await Promise.all(promises);
-    if (new_photos.length) {
-      updateData.photos = new_photos;
+    if (item) {
+      return { success: true, message: 'Item updated', item: item };
     } else {
-      delete updateData.photos;
+      throw Error('Cannot update item');
     }
-    try {
-      const item = await Item.findOneAndUpdate({ _id: id }, updateData, {
-        useFindAndModify: false,
-        returnDocument: 'after',
-      });
-      if (item) {
-        return { success: true, message: 'Item updated', item: item };
-      } else {
-        throw Error('Cannot update item');
-      }
-    } catch (err) {
-      console.log(err);
-      return { success: false, message: err };
-    }
-  } else {
-    //if no new images to add then remove the empty photos list and continue updating
-    delete updateData.photos;
-    try {
-      const item = await Item.findOneAndUpdate({ _id: id }, updateData, {
-        useFindAndModify: false,
-        returnDocument: 'after',
-      });
-
-      if (item) {
-        results = { success: true, message: 'Item updated', item: item };
-      } else {
-        throw Error('Cannot update item');
-      }
-    } catch (err) {
-      console.log(err);
-      results = { success: false, message: err };
-    }
+  } catch (err) {
+    console.log(err);
+    return { success: false, message: err };
   }
+  // } else {
+  //   //if no new images to add then remove the empty photos list and continue updating
+  //   delete updateData.photos;
+  //   try {
+  //     const item = await Item.findOneAndUpdate({ _id: id }, updateData, {
+  //       useFindAndModify: false,
+  //       returnDocument: 'after',
+  //     });
+
+  //     if (item) {
+  //       results = { success: true, message: 'Item updated', item: item };
+  //     } else {
+  //       throw Error('Cannot update item');
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //     results = { success: false, message: err };
+  //   }
+  // }
   return results;
 };
 
