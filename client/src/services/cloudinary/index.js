d455dafa53228e436bad437f554f79a31a87cacc
Haydn Appleby <haydnba@pm.me>
2023-10-01 22:46:35 +0100

    adds client bud delete handler plus better error handling and comments

--- client/src/services/cloudinary/index.js
+++ client/src/services/cloudinary/index.js
@@ -9,6 +9,7 @@ export const getImageUrl = ({ cloudName, publicId, transformations }) => {
  * Get the secure signed url from backend...
  */
 export const getSignedUrl = async (params, token) => {
+  console.log({ params, token });
   try {
     const options = {
       method: 'POST',
@@ -24,16 +25,51 @@ export const getSignedUrl = async (params, token) => {
       (res) => res.json()
     );
 
+    if (result.success === false) {
+      throw new Error(result.message);
+    }
+
     return result;
-  } catch (error) {
-    // TODO
-    console.error(`Error in cloudinary get signed url: ${error}`);
-    return error;
+  } catch (e) {
+    console.error(`Error in cloudinary get signed url: ${e}`);
+    return { error: e };
+  }
+};
+
+/**
+ * Trigger buld deletion via the admin api for cleanup actions.
+ */
+export const bulkDelete = async (ids, token) => {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'x-access-token': token,
+      },
+      body: JSON.stringify({ publicIds: ids }),
+    };
+
+    const result = await fetch(
+      '/api/cloudinary/delete_resources',
+      options
+    ).then((res) => res.json());
+
+    if (result.success === false) {
+      throw new Error(result.message);
+    }
+
+    return result;
+  } catch (e) {
+    console.error(`Error in cloudinary bulk delete resources: ${e}`);
+    return { error: e };
   }
 };
 
 /**
  * https://cloudinary.com/documentation/image_upload_api_reference#signed_upload_syntax
+ * https://cloudinary.com/documentation/image_upload_api_reference#error_handling
  */
 export const assetUpload = async (formData, cloudname) => {
   try {
@@ -45,15 +81,15 @@ export const assetUpload = async (formData, cloudname) => {
     );
 
     return result;
-  } catch (error) {
-    // TODO
-    console.error(`Error in cloudinary asset upload: ${error}`);
-    return error;
+  } catch (e) {
+    console.error(`Error in cloudinary asset upload: ${e}`);
+    return { error: e };
   }
 };
 
 /**
  * https://cloudinary.com/documentation/image_upload_api_reference#destroy_syntax
+ * https://cloudinary.com/documentation/image_upload_api_reference#error_handling
  */
 export const assetDestroy = async (formData, cloudname) => {
   try {
@@ -68,8 +104,8 @@ export const assetDestroy = async (formData, cloudname) => {
     );
 
     return result;
-  } catch (error) {
-    console.error(`Error in cloudinary asset destroy: ${error}`);
-    return error;
+  } catch (e) {
+    console.error(`Error in cloudinary asset destroy: ${e}`);
+    return { error: e };
   }
 };
