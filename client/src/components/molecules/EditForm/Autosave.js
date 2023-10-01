748288c72e11cda5af8eff77dab554c06ca984b0
Haydn Appleby <haydnba@pm.me>
2023-10-01 22:47:45 +0100

    adds an autosaver componend for inclusion in formik forms where automatic saving is needed (item edit form)

---
+++ client/src/components/molecules/EditForm/Autosave.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { useFormikContext } from 'formik';
+import { Notification } from '../../atoms';
+
+const debounce = (func, wait) => {
+  let timeout;
+
+  return function (...args) {
+    const context = this;
+
+    if (timeout) clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      timeout = null;
+      func.apply(context, args);
+    }, wait);
+  };
+};
+
+const notice = ['Success!', 'changes saved', 'success'];
+
+export const AutoSave = ({ delay = 2000 }) => {
+  const initRef = React.useRef(true);
+
+  const { errors, submitForm, values } = useFormikContext();
+
+  const handleSave = React.useCallback(() => {
+    Notification(...notice);
+    submitForm();
+  }, [submitForm]);
+
+  const debouncedSubmit = React.useMemo(
+    () => debounce(handleSave, delay),
+    [handleSave, delay]
+  );
+
+  React.useEffect(() => {
+    initRef.current || Object.keys(errors)[0] || debouncedSubmit();
+  }, [debouncedSubmit, errors, values]);
+
+  React.useEffect(() => {
+    initRef.current = false;
+  }, []);
+
+  return null;
+};
