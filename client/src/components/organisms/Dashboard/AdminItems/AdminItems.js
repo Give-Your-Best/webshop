22f302bb5a0f5745c6a2ed04281daa1326d15e27
Haydn Appleby <haydnba@pm.me>
2023-10-26 00:13:51 +0100

    major refactor of the admin items component with removal of tabs, lazier loading of data, new UI for selecting the view type and an autocomplete search for shoppers/donors etc.

--- client/src/components/organisms/Dashboard/AdminItems/AdminItems.js
+++ client/src/components/organisms/Dashboard/AdminItems/AdminItems.js
@@ -1,28 +1,23 @@
 import React, { useContext, useState, useEffect, useRef } from 'react';
-import { Modal } from 'antd';
+import { AutoComplete, Modal, Select, Space } from 'antd';
 import { AppContext } from '../../../../context/app-context';
 import { ItemCardLong, ItemsCollapsedList } from '../../../molecules';
-import {
-  StyledTab,
-  StyledTabList,
-  StyledTabs,
-  StyledTabPanel,
-} from './AdminItems.styles';
 import { getAdminItems, deleteItem } from '../../../../services/items';
 import { getTags } from '../../../../services/tags';
+import { getPublicUsers } from '../../../../services/user';
 import { tabList } from '../../../../utils/helpers';
 
 export const AdminItems = () => {
   const { token, user } = useContext(AppContext);
   const mountedRef = useRef(true);
   const [tags, setTags] = useState([]);
+  const [users, setUsers] = useState([]);
   const [items, setItems] = useState([]);
-  const [pastItems, setPastItems] = useState([]);
+  const [view, setView] = useState('current');
   const [totalItems, setTotalItems] = useState(0);
-  const [totalPastItems, setTotalPastItems] = useState(0);
   const [currentItemsPage, setCurrentItemsPage] = useState(1);
-  const [currentPastItemsPage, setCurrentPastItemsPage] = useState(1);
   // TODO - we need to look at the naming here it is getting confusing...
+  const [currentUser, setCurrentUser] = useState({});
 
   // Set this constant for now - we might want to allow adjustment later
   const limit = 10;
@@ -35,16 +30,17 @@ export const AdminItems = () => {
       className: 'modalStyle',
       onOk() {
         deleteItem(id, token).then(() => {
-          setItems(
-            items.filter((item) => {
-              return item._id !== id;
-            })
-          );
-          setPastItems(
-            pastItems.filter((item) => {
-              return item._id !== id;
-            })
-          );
+          // TODO need to update the component?
+          // setItems(
+          //   items.filter((item) => {
+          //     return item._id !== id;
+          //   })
+          // );
+          // setPastItems(
+          //   pastItems.filter((item) => {
+          //     return item._id !== id;
+          //   })
+          // );
         });
       },
     });
@@ -67,13 +63,27 @@ export const AdminItems = () => {
       }
     });
 
-    const fetchAllTags = async () => {
-      if (!mountedRef.current) return null;
+    const fetchTags = async () => {
       const tags = await getTags(token);
       setTags(tags);
     };
 
-    fetchAllTags();
+    const fetchUsers = async () => {
+      const data = await getPublicUsers(token);
+
+      setUsers(
+        data.map((d) => ({
+          label: `${d.firstName} ${d.lastName}`.trim(),
+          value: d._id,
+          type: d.kind,
+        }))
+      );
+    };
+
+    if (mountedRef.current) {
+      fetchTags();
+      fetchUsers();
+    }
 
     return () => {
       // cleanup
@@ -83,65 +93,121 @@ export const AdminItems = () => {
 
   useEffect(() => {
     const fetchItems = async () => {
-      const { total, items } = await getAdminItems(
-        true, // Current items
+      const { total, items } = await getAdminItems({
+        isCurrent: view === 'current', // Current items
         limit,
-        currentItemsPage
-      );
+        page: currentItemsPage,
+        donorId: currentUser.type === 'donor' ? currentUser.value : undefined,
+        shopperId:
+          currentUser.type === 'shopper' ? currentUser.value : undefined,
+      });
 
       setItems(items);
       setTotalItems(total);
     };
 
     fetchItems();
-  }, [token, user, currentItemsPage]);
+  }, [token, user, view, currentItemsPage, currentUser]);
 
-  useEffect(() => {
-    const fetchPastItems = async () => {
-      const { total, items } = await getAdminItems(
-        false, // Past items
-        limit,
-        currentPastItemsPage
-      );
+  const handleSelectUser = (_value, option) => {
+    setCurrentItemsPage(1);
+    setCurrentUser(option);
+  };
 
-      setPastItems(items);
-      setTotalPastItems(total);
-    };
+  const handleClearUser = () => {
+    setCurrentItemsPage(1);
+    setCurrentUser({});
+  };
 
-    fetchPastItems();
-  }, [token, user, currentPastItemsPage]);
+  const handleFilterOptions = (inputValue, option) => {
+    const re = new RegExp(inputValue, 'i');
+    return re.test(option.label);
+  };
+
+  const handleSelectView = (view) => {
+    setCurrentItemsPage(1);
+    setView(view);
+  };
+
+  const handleSetPage = (page) => setCurrentItemsPage(page);
 
   return (
-    <StyledTabs forceRenderTabPanel={true}>
-      <StyledTabList>
-        <StyledTab className="itemslist">Items</StyledTab>
-        <StyledTab>My Past Items</StyledTab>
-      </StyledTabList>
+    <>
+      <Space>
+        <Select
+          size="large"
+          style={{ width: 150 }}
+          placeholder="Items View"
+          defaultValue="current"
+          onSelect={handleSelectView}
+          options={[
+            {
+              label: 'Current Items',
+              value: 'current',
+            },
+            {
+              label: 'Past Items',
+              value: 'past',
+            },
+          ]}
+        />
 
-      <StyledTabPanel>
-        <ItemsCollapsedList
-          data={items}
-          total={totalItems}
-          current={currentItemsPage}
-          onChange={(page) => setCurrentItemsPage(page)}
-          expandRow={editForm}
-          handleDelete={handleDelete}
-          admin={true}
-          allTags={tags}
+        <AutoComplete
+          value={(currentUser || {}).label}
+          options={users}
+          style={{ width: 300 }}
+          size="large"
+          onClear={handleClearUser}
+          onSelect={handleSelectUser}
+          filterOption={handleFilterOptions}
+          placeholder="Shopper or Donor"
+          allowClear
         />
-      </StyledTabPanel>
-      <StyledTabPanel>
-        <ItemsCollapsedList
-          data={pastItems}
-          total={totalPastItems}
-          current={currentPastItemsPage}
-          onChange={(page) => setCurrentPastItemsPage(page)}
-          expandRow={editForm}
-          handleDelete={handleDelete}
-          admin={true}
-          allTags={tags}
-        />
-      </StyledTabPanel>
-    </StyledTabs>
+      </Space>
+
+      <ItemsCollapsedList
+        data={items}
+        total={totalItems}
+        current={currentItemsPage}
+        onChange={handleSetPage}
+        expandRow={editForm}
+        handleDelete={handleDelete}
+        admin={true}
+        allTags={tags}
+      />
+    </>
+    // <StyledTabs forceRenderTabPanel={true}>
+    //   <StyledTabList>
+    //     <StyledTab className="itemslist">Items</StyledTab>
+    //     <StyledTab>My Past Items</StyledTab>
+    //   </StyledTabList>
+
+    //   <StyledTabPanel>
+    //     <ItemsCollapsedList
+    //       data={items}
+    //       total={totalItems}
+    //       current={currentItemsPage}
+    //       onFilter={(...data) => console.log(...data)}
+    //       onChange={(page) => setCurrentItemsPage(page)}
+    //       expandRow={editForm}
+    //       handleDelete={handleDelete}
+    //       admin={true}
+    //       allTags={tags}
+    //     />
+    //   </StyledTabPanel>
+    //   <StyledTabPanel>
+    //     <ItemsCollapsedList
+    //       data={pastItems}
+    //       total={totalPastItems}
+    //       current={currentPastItemsPage}
+    //       onFilter={console.log}
+    //       onChange={(page) => setCurrentPastItemsPage(page)}
+    //       expandRow={editForm}
+    //       handleDelete={handleDelete}
+    //       admin={true}
+    //       allTags={tags}
+    //     />
+    //   </StyledTabPanel>
+    // </StyledTabs>
   );
 };
--- client/src/services/items/getAdminItems.js
+++ client/src/services/items/getAdminItems.js
@@ -1,8 +1,16 @@
-export const getAdminItems = async (isCurrent, limit = 10, page = 1) => {
+export const getAdminItems = async ({
+  isCurrent,
+  limit = 10,
+  page = 1,
+  donorId = undefined,
+  shopperId = undefined,
+}) => {
   const response = await fetch(
     `/api/items/admin?isCurrent=${Boolean(
       isCurrent
-    )}&limit=${limit}&page=${page}`,
+    )}&limit=${limit}&page=${page}${donorId ? `&donorId=${donorId}` : ''}${
+      shopperId ? `&shopperId=${shopperId}` : ''
+    }`,
     {
       headers: {
         'Content-Type': 'application/json',
