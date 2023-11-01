189ebe34dd2480de3a273f6f2ffe38558ee426f5
Haydn Appleby <haydnba@pm.me>
2023-11-01 00:45:36 +0000

    adds filtering by category and status and disabled the use of tags (WIP)

--- client/src/components/organisms/Dashboard/AdminItems/AdminItems.js
+++ client/src/components/organisms/Dashboard/AdminItems/AdminItems.js
@@ -5,24 +5,26 @@ import React, {
   useEffect,
   useRef,
 } from 'react';
-import { AutoComplete, Modal, Select, Space } from 'antd';
+import { AutoComplete, Menu, Modal, Select, Space } from 'antd';
 import { AppContext } from '../../../../context/app-context';
 import { ItemCardLong, ItemsCollapsedList } from '../../../molecules';
 import { getAdminItems, deleteItem } from '../../../../services/items';
 import { getTags } from '../../../../services/tags';
 import { getPublicUsers } from '../../../../services/user';
 import { tabList } from '../../../../utils/helpers';
+import { categories } from '../../../../utils/constants';
+import { adminAllItemStatus } from '../../../atoms/ProgressBar/constants';
 
 export const AdminItems = () => {
   const { token, user } = useContext(AppContext);
   const mountedRef = useRef(true);
-  const [tags, setTags] = useState([]);
   const [users, setUsers] = useState([]);
   const [items, setItems] = useState([]);
   const [view, setView] = useState('current');
   const [totalItems, setTotalItems] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const [currentUser, setCurrentUser] = useState({});
+  const [conditions, setConditions] = useState([]);
 
   // Set this constant for now - we might want to allow adjustment later
   const limit = 10;
@@ -42,17 +44,32 @@ export const AdminItems = () => {
     const donorId = type === 'donor' ? value : undefined;
     const shopperId = type === 'shopper' ? value : undefined;
 
+    const { category, status } = conditions.reduce(
+      (acc, cur) => {
+        const [k, v] = cur.split(':');
+        acc[k] = acc[k] || [];
+        acc[k].push(v);
+        return acc;
+      },
+      {
+        category: undefined,
+        status: undefined,
+      }
+    );
+
     const { total, items } = await getAdminItems({
       isCurrent: view === 'current',
       page: currentPage,
       limit,
       donorId,
       shopperId,
+      category,
+      status,
     });
 
     setItems(items);
     setTotalItems(total);
-  }, [currentPage, currentUser, view]);
+  }, [conditions, currentPage, currentUser, view]);
 
   useEffect(() => {
     var tabs = tabList(user);
@@ -63,13 +80,15 @@ export const AdminItems = () => {
       }
     });
 
-    const fetchTags = async () => {
-      const tags = await getTags(token);
-      setTags(tags);
-    };
+    // const fetchTags = async () => {
+    //   const tags = await getTags(token);
+    //   console.log('HAHAH', tags);
+    //   setAllTags(tags);
+    // };
 
     const fetchUsers = async () => {
       const data = await getPublicUsers(token);
+      console.log('HOHOHO', data);
       setUsers(
         data.map((d) => ({
           label: `${d.firstName} ${d.lastName}`.trim(),
@@ -80,7 +99,7 @@ export const AdminItems = () => {
     };
 
     if (mountedRef.current) {
-      fetchTags();
+      // fetchTags();
       fetchUsers();
     }
 
@@ -115,8 +134,17 @@ export const AdminItems = () => {
   };
 
   const handleSelectView = (view) => {
+    setConditions([]);
     setCurrentPage(1);
     setView(view);
+  };
+
+  const handleSelectFilter = (e) => {
+    setConditions(
+      conditions.includes(e.key)
+        ? conditions.filter((i) => i !== e.key)
+        : [...conditions, e.key]
+    );
   };
 
   const handleSetPage = (page) => setCurrentPage(page);
@@ -137,6 +165,7 @@ export const AdminItems = () => {
         <Select
           size="large"
           style={{ width: 150 }}
+          value={view}
           placeholder="Items View"
           defaultValue="current"
           onSelect={handleSelectView}
@@ -163,6 +192,43 @@ export const AdminItems = () => {
           placeholder="Shopper or Donor"
           allowClear
         />
+
+        <Menu
+          triggerSubMenuAction="click"
+          onClick={handleSelectFilter}
+          selectedKeys={conditions}
+          multiple
+          mode="horizontal"
+          items={[
+            {
+              label: 'Category',
+              key: 'category',
+              children: categories.map((c) => ({
+                label: c.name,
+                key: `category:${c.id}`,
+              })),
+            },
+          ]}
+        />
+
+        <Menu
+          triggerSubMenuAction="click"
+          onClick={handleSelectFilter}
+          selectedKeys={conditions}
+          multiple
+          mode="horizontal"
+          items={[
+            {
+              label: 'Status',
+              key: 'status',
+              children: adminAllItemStatus.map((s) => ({
+                label: s.statusText,
+                key: `status:${s.status}`,
+                disabled: view === 'past' || s.status === 'received',
+              })),
+            },
+          ]}
+        />
       </Space>
 
       <ItemsCollapsedList
@@ -173,7 +239,7 @@ export const AdminItems = () => {
         expandRow={editForm}
         handleDelete={handleDeleteItem}
         admin={true}
-        allTags={tags}
+        allTags={[]}
       />
     </>
   );
