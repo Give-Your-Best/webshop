5a014a6ab3c7eae6b0cd5b383adb12b0d44eeda5
Haydn Appleby <haydnba@pm.me>
2023-11-09 21:50:19 +0000

    update the bugsnag util accordingly

--- server/utils/bugsnag.js
+++ server/utils/bugsnag.js
@@ -1,6 +1,10 @@
 const Bugsnag = require('@bugsnag/js');
+const BugsnagPluginExpress = require('@bugsnag/plugin-express');
 const config = require('../config/bugsnag');
 
-Bugsnag.start(config);
+Bugsnag.start({
+  ...config,
+  plugins: [BugsnagPluginExpress],
+});
 
 module.exports = Bugsnag;
