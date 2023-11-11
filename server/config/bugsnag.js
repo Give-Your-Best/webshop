97f046bcbca4acc09afe88146a3f8f353cf6e950
Haydn Appleby <haydnba@pm.me>
2023-11-11 21:16:58 +0000

    update bugsnag config and pass envs to build scripts in package.json fo use in client build

--- server/config/bugsnag.js
+++ server/config/bugsnag.js
@@ -1,6 +1,9 @@
 const env = require('./environment');
 
 module.exports = {
-  apiKey: env === 'production' ? '' : 'fb3f2d4e229463de31f3ed1a61918875', // TODO
+  apiKey:
+    env === 'production'
+      ? 'fb3f2d4e229463de31f3ed1a61918875' // TODO - not yet created prod
+      : 'fb3f2d4e229463de31f3ed1a61918875',
   releaseStage: env,
 };
