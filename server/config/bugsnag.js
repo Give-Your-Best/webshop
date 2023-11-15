82b6d2a85bb7a05d7a206716b76c9dc8ecfe5d72
Haydn Appleby <haydnba@pm.me>
2023-11-15 15:35:39 +0000

    add the prod bugsnag key

--- server/config/bugsnag.js
+++ server/config/bugsnag.js
@@ -3,7 +3,7 @@ const env = require('./environment');
 module.exports = {
   apiKey:
     env === 'production'
-      ? 'fb3f2d4e229463de31f3ed1a61918875' // TODO - not yet created prod
+      ? '38171f6c0c506918a19deac49c1a5a19'
       : 'fb3f2d4e229463de31f3ed1a61918875',
   releaseStage: env,
   enabledReleaseStages: ['production', 'staging'],
