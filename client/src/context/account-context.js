b3a03a3517257a8e4deb00309c4c37190441b0e4
Haydn Appleby <haydnba@pm.me>
2023-11-28 22:06:19 +0000

    remove the unused user checks in the account context

--- client/src/context/account-context.js
+++ client/src/context/account-context.js
@@ -1,17 +1,11 @@
 import * as React from 'react';
 
-import { AppContext } from './app-context';
-
 export const AccountContext = React.createContext();
 
 export const AccountProvider = ({ children }) => {
-  const { user } = React.useContext(AppContext);
-
   const [allTags, setAllTags] = React.useState(null);
   const [allUsers, setAllUsers] = React.useState(null);
   const [currentUser, setCurrentUser] = React.useState(null);
-
-  console.log(user); // Depending on user type we will expose data or not
 
   const value = React.useMemo(
     () => ({
