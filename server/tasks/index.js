6caaf81c1f94818f167b6b0d3f72e7b79a63c485
Haydn Appleby <haydnba@pm.me>
2023-12-06 00:01:26 +0000

    tidying

--- server/tasks/index.js
+++ server/tasks/index.js
@@ -4,67 +4,64 @@ const { getStatusReminderItems } = require('../services/items');
 
 const emailTemplate = (content) => `
 <html>
-    <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet"> 
-        <style>
-            p {
-                color: #BA191A;
-                font-size: 20px;
-                font-family: Lato;
-                text-align: center;
-            }
-            a {
-                border: 2px solid #A21010;
-                padding: 0.3rem 1.2rem;
-                font-size: 20px;
-                color: #BA191A;
-                font-family: Lato;
-                background-color: #FAD22A;
-                margin: 30px auto;
-                width: 150px;
-                text-align: center;
-                text-decoration: none;
-                display: block;
-            }
-        </style>
-    </head>
-    <body style="width:100%;margin:0;background-color:#FFFBFC;">
-        <div style="height:125px;background-color:#FAD6DF;padding:10px 0 50px 0;display:block;width:100%;">
-            <img style=" width:100px;height:100px;margin:auto;display:block;" alt='logo' src='https://res.cloudinary.com/hnlrfgzzh/image/upload/v1655378744/GYB_Logos_copy-07_z1ke0l_td1xmj.png' />
+  <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet"> 
+    <style>
+      p {
+        color: #BA191A;
+        font-size: 20px;
+        font-family: Lato;
+        text-align: center;
+      }
+      a {
+        border: 2px solid #A21010;
+        padding: 0.3rem 1.2rem;
+        font-size: 20px;
+        color: #BA191A;
+        font-family: Lato;
+        background-color: #FAD22A;
+        margin: 30px auto;
+        width: 150px;
+        text-align: center;
+        text-decoration: none;
+        display: block;
+      }
+    </style>
+  </head>
+  <body style="width:100%;margin:0;background-color:#FFFBFC;">
+    <div style="height:125px;background-color:#FAD6DF;padding:10px 0 50px 0;display:block;width:100%;">
+          <img style=" width:100px;height:100px;margin:auto;display:block;" alt='logo' src='https://res.cloudinary.com/hnlrfgzzh/image/upload/v1655378744/GYB_Logos_copy-07_z1ke0l_td1xmj.png' />
+    </div>
+    <div style="display:flex">
+      <div style="width:15%;margin:0;display:block"></div>
+      <div style="width:70%;display:block;margin:-40px 0 0 0;background-color:white;border:2px solid #A21010;border-radius: 30px;padding: 30px;color:#BA191A;font-size:20px;font-family:Lato;text-align:center;">
+        ${content}
+        <div style="margin-top:30px;">
+          <p>Thanks!</p>
+          <p>Give Your Best Team! x</p>
         </div>
-        <div style="display:flex">
-            <div style="width:15%;margin:0;display:block"></div>
-            <div style="width:70%;display:block;margin:-40px 0 0 0;background-color:white;border:2px solid #A21010;border-radius: 30px;padding: 30px;color:#BA191A;font-size:20px;font-family:Lato;text-align:left;">
-                ${content}
-                <div style="margin-top:30px;">
-                    <p>Thanks!</p>
-                    <p>Give Your Best Team! x</p>
-                </div>
-            </div>
-            <div style="width:15%;margin:0;display:block"></div>
-        </div>
-    </body>
+      </div>
+      <div style="width:15%;margin:0;display:block"></div>
+    </div>
+  </body>
 </html>
 `;
 
-// TODO
 const renderEmailProperties = (user, items, interval) => {
   const period = {
     7: '1 week',
     14: '2 weeks',
   }[interval];
 
-  const pluralise = items.length > 1;
+  const plural = items.length > 1;
 
   const subject = {
     donor: `${period} since your ${
-      pluralise ? 'items were' : 'item was'
+      plural ? 'items were' : 'item was'
     } shopped!`,
-    shopper: `${period} since your ${
-      pluralise ? 'items were' : 'item was'
-    } sent!`,
+    shopper: `${period} since your ${plural ? 'items were' : 'item was'} sent!`,
   }[user.kind];
 
   const name = `${user.firstName} ${user.lastName}`.trim();
@@ -72,38 +69,38 @@ const renderEmailProperties = (user, items, interval) => {
   const content = {
     donor: `
 <div style="margin-top:30px;">
-    <p>Dear ${name},</p>
-    <p>It is ${period} since your ${
-      pluralise ? 'items were' : 'item was'
-    } shopped:</p>
-    ${
-      pluralise
-        ? `<ul>${items
-            .map((i) => `<li><strong>${i.name}</strong></li>`)
-            .join('\n')}</ul>`
-        : `<strong>${items[0].name}</strong>`
-    }
-    <p>Please remember to mark as sent once ${
-      pluralise ? "they're" : "it's"
-    } in the post!</p>
+  <p>Dear ${name},</p>
+  <p>It is ${period} since your ${
+    plural ? 'items were' : 'item was'
+  } shopped:</p>
+  ${
+    plural
+      ? `<ul>${items
+          .map((i) => `<li><strong>${i.name}</strong></li>`)
+          .join('\n')}</ul>`
+      : `<strong>${items[0].name}</strong>`
+  }
+  <p>Please remember to mark as sent once ${
+    plural ? "they're" : "it's"
+  } in the post!</p>
 </div>
   `,
     shopper: `
 <div style="margin-top:30px;">
-    <p>Dear ${name},</p>
-    <p>It is ${period} since your ${
-      pluralise ? 'items were' : 'item was'
-    } shipped:</p>
-    ${
-      pluralise
-        ? `<ul>${items
-            .map((i) => `<li><strong>${i.name}</strong></li>`)
-            .join('\n')}</ul>`
-        : `<strong>${items[0].name}</strong>`
-    }
-    <p>Please remember to mark as received once ${
-      pluralise ? "they've" : "it's"
-    } arrived!</p>
+  <p>Dear ${name},</p>
+  <p>It is ${period} since your ${
+    plural ? 'items were' : 'item was'
+  } shipped:</p>
+  ${
+    plural
+      ? `<ul>${items
+          .map((i) => `<li><strong>${i.name}</strong></li>`)
+          .join('\n')}</ul>`
+      : `<strong>${items[0].name}</strong>`
+  }
+  <p>Please remember to mark as received once ${
+    plural ? "they've" : "it's"
+  } arrived!</p>
 </div>
   `,
   }[user.kind];
@@ -115,7 +112,7 @@ const renderEmailProperties = (user, items, interval) => {
  * Dispatch reminder emails for status updates on items either shoppe (donor
  * user reminders) or shipped (shopper user reminders).
  */
-const send_order_status_reminders = async () => {
+exports.send_order_status_reminders = async () => {
   const settings = [
     // 1 week since item shopped, please confirm sent
     {
@@ -165,18 +162,11 @@ const send_order_status_reminders = async () => {
         s.interval
       );
 
-      // TODO return [subject, content, email, name];
-      return [subject, emailTemplate(content), 'email@example.com', name];
+      return [subject, emailTemplate(content), u.email, name];
     });
 
-    const results = await Promise.all(reminders.map((r) => sendMail(...r)));
+    await Promise.all(reminders.map((r) => sendMail(...r)));
 
-    // Will want to log something here or maybe write a log to db...
-
-    console.log(results);
+    // TODO - we should log the result here
   }
-};
-
-module.exports = {
-  send_order_status_reminders,
 };
