60b3779ead327083ad1ab2a65d6ae20a65e2e761
Haydn Appleby <haydnba@pm.me>
2023-12-23 22:51:03 +0000

    integrates client and backend

--- client/src/components/organisms/Banners/Banner.js
+++ client/src/components/organisms/Banners/Banner.js
@@ -1,27 +1,37 @@
 import * as React from 'react';
 import { StyledAlert } from './Banner.styles';
 
+/**
+ * TODO this should move to the services dir...
+ */
+const getBannerAlerts = async () => {
+  const response = await fetch(`/api/banners`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const body = await response.json();
+  return body;
+};
+
 export const Banner = () => {
+  const [banners, setBanners] = React.useState([]);
+
+  React.useEffect(() => {
+    getBannerAlerts().then(setBanners);
+  }, []);
+
   return (
     <div>
-      {[
-        {
-          _id: '65874e5d3cb54687fa82664a',
-          type: 'info',
-          message: 'Please note!',
-          description:
-            "Our team are on a break for the festive season and will be back on Jan 8th. The shop is still open but be aware that messages won't be answered and some items won't be shipped until January.",
-        },
-      ].map((item) => {
+      {banners.map((item) => {
         return (
           <div key={item._id}>
             <StyledAlert
-              message={item.message}
-              description={item.description}
-              type={item.type}
+              message={item.message || ''}
+              description={item.description || ''}
+              type={item.type || 'info'}
               closable
               showIcon
-              // onClose={onClose}
             />
           </div>
         );
