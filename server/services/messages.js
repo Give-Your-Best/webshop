dc3ad94391c5539e35bef90f2ee81f7afa650ad1
Haydn Appleby <haydnba@pm.me>
2024-02-03 22:00:05 +0000

    switch back to using the service pattern for new messages methods...

--- server/services/messages.js
+++ server/services/messages.js
@@ -1,6 +1,105 @@
 const Message = require('../models/Message');
 const BSON = require('bson');
 
+// TODO
+const getAdminThreads = async (type) => {
+  const threads = await Message.find({ type })
+    .populate('user')
+    .populate('messages.sender')
+    .populate('messages.recipient')
+    .sort({ updatedAt: -1 });
+
+  return threads;
+};
+
+// TODO
+const getUserThreads = async (user) => {
+  const threads = await Message.find({ user })
+    .populate('user')
+    .populate('messages.sender')
+    .populate('messages.recipient')
+    .sort({ updatedAt: -1 });
+
+  return threads;
+};
+
+// TODO
+const getThreadSummaries = async (user) => {
+  const { kind } = user;
+
+  const $match = {
+    ...(kind === 'admin' ? {} : { user: user._id }),
+  };
+
+  const $project = {
+    threadId: '$threadId',
+    userType: '$type',
+    messages: { $size: '$messages' },
+    unviewed: {
+      $size: {
+        $filter: {
+          input: '$messages',
+          cond: {
+            $eq: ['$$this.viewed', false],
+          },
+        },
+      },
+    },
+  };
+
+  const data = await Message.aggregate([
+    {
+      $match,
+    },
+    {
+      $project,
+    },
+  ]);
+
+  return data;
+};
+
+// TODO
+const markMessageAsViewed = async (id, messageIds) => {
+  try {
+    const thread = await Message.findOneAndUpdate(
+      { _id: id, 'messages._id': { $in: messageIds } },
+      { $set: { 'messages.$.viewed': true } },
+      { new: true }
+    );
+    if (thread) {
+      return { success: true, message: 'marked as viewed', thread: thread };
+    } else {
+      throw Error('Cannot update message');
+    }
+  } catch (err) {
+    console.log(err);
+    return { success: false, message: err };
+  }
+};
+
+// TODO
+const upsertThread = async (data) => {
+  const threadId = data.threadId || new BSON.ObjectId();
+
+  const { messages, ...rest } = data;
+
+  const thread = await Message.findOneAndUpdate(
+    { threadId },
+    { ...rest, $push: { messages } },
+    { new: true, upsert: true }
+  )
+    .populate('user')
+    .populate('messages.sender')
+    .populate('messages.recipient')
+    .exec();
+
+  return thread;
+};
+
+/**
+ * DEPRECATED...
+ */
 const getMessages = async (type, userId) => {
   try {
     if (userId === 'all') {
@@ -31,47 +130,9 @@ const getMessages = async (type, userId) => {
   }
 };
 
-const markMessageAsViewed = async (id, messageIds) => {
-  try {
-    const thread = await Message.findOneAndUpdate(
-      { _id: id, 'messages._id': { $in: messageIds } },
-      { $set: { 'messages.$.viewed': true } },
-      { new: true }
-    );
-    if (thread) {
-      return { success: true, message: 'marked as viewed', thread: thread };
-    } else {
-      throw Error('Cannot update message');
-    }
-  } catch (err) {
-    console.log(err);
-    return { success: false, message: err };
-  }
-};
-
-const upsertThread = async function (data) {
-  try {
-    const threadId = data.threadId || new BSON.ObjectId();
-
-    const { messages, ...rest } = data;
-
-    const thread = await Message.findOneAndUpdate(
-      { threadId },
-      { ...rest, $push: { messages } },
-      { new: true, upsert: true }
-    )
-      .populate('user')
-      .populate('messages.sender')
-      .populate('messages.recipient')
-      .exec();
-
-    return thread;
-  } catch (e) {
-    // TODO
-    console.log(e);
-  }
-};
-
+/**
+ * DEPRECATED...
+ */
 const createMessage = async (data) => {
   if (!data.threadId || data.threadId === '') {
     //if no thread id then add a new one
@@ -117,8 +178,12 @@ const createMessage = async (data) => {
 };
 
 module.exports = {
+  getAdminThreads,
+  getUserThreads,
+  getThreadSummaries,
+  upsertThread,
+
   getMessages,
   createMessage,
   markMessageAsViewed,
-  upsertThread,
 };
