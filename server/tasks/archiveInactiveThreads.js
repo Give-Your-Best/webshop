34b8bd3b68422e82ec933e4ceec3a024132c42be
Haydn Appleby <haydnba@pm.me>
2024-02-17 22:42:06 +0000

    build out the task runner

--- server/tasks/archiveInactiveThreads.js
+++ server/tasks/archiveInactiveThreads.js
@@ -1,4 +1,14 @@
+const { archiveThreads, getStaleThreads } = require('../services/messages');
+
 module.exports = async (logger) => {
-  console.log('This is the archive inactive message threads runner module');
+  // console.log('This is the archive inactive message threads runner module');
   logger.info('This is the archive inactive message threads runner module');
+
+  const archivableThreads = await getStaleThreads();
+
+  console.log(archivableThreads.length);
+
+  const threadIds = archivableThreads.map((t) => t.threadId);
+
+  await archiveThreads(threadIds);
 };
