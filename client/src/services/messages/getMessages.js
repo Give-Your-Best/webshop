8e4bef25642c0c031518e28291a574a4487634e3
Haydn Appleby <haydnba@pm.me>
2024-04-19 23:57:23 +0100

    update the api client for archived...

--- client/src/services/messages/getMessages.js
+++ client/src/services/messages/getMessages.js
@@ -1,10 +1,14 @@
-export const getMessages = async (type, id, token) => {
-  const response = await fetch(`/api/messages?type=${type}&id=${id}`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'x-access-token': token,
-    },
-  });
+export const getMessages = async (type, id, archived = false, token) => {
+  console.log(archived);
+  const response = await fetch(
+    `/api/messages?type=${type}&id=${id}&archived=${archived}`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-access-token': token,
+      },
+    }
+  );
   const body = await response.json();
   if (response.status !== 200) {
     throw Error(body.message);
