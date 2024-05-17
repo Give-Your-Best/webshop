2c21f74d4659dc2126f97b20863d60d6f9971544
MemorySounds <r.sahakyan1@gmail.com>
2024-05-17 17:00:53 +0200

    temporary endpoints established for generating and downloading report

--- server/routes/api/statistics.js
+++ server/routes/api/statistics.js
@@ -4,6 +4,8 @@ const {
   getAllStatistics,
   getReportData,
 } = require('../../services/statistics');
+const { generateReport } = require('../../services/report/reportFactory');
+const Report = require('../../models/report');
 
 // get settings endpoint api/statistics
 router.get('/', async (req, res) => {
@@ -17,6 +19,35 @@ router.get('/report', async (req, res) => {
   let to = req.query.to || '';
   const data = await getReportData(from, to);
   res.json(data);
+});
+
+// generate historic report
+router.get('/historic', async (req, res) => {
+  const response = await generateReport();
+  res.json(response);
+});
+
+router.get('/download/:reportName', async (req, res) => {
+  try {
+    // Fetch the report from the database
+    const report = await Report.findOne({ name: req.params.reportName });
+
+    // Set the response headers
+    res.setHeader(
+      'Content-Type',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    );
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename=${report.reportName}.xlsx`
+    );
+
+    // Send the report data as a response
+    res.send(report.data);
+  } catch (error) {
+    console.error(`Error in downloading report: ${error}`);
+    res.status(500).send('An error occurred while downloading the report');
+  }
 });
 
 module.exports = router;
