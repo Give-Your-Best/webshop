4fe0ba3bc31bb4075e07c62fcb7602e4f6f3b103
MemorySounds <r.sahakyan1@gmail.com>
2024-05-20 21:14:22 +0200

    email notification added to report generation need - needs testing

--- server/tasks/generateHistoricReport.js
+++ server/tasks/generateHistoricReport.js
@@ -1,9 +1,63 @@
-const generateReport = require('../services/report/generateReport');
+const {
+  generateReport,
+  getLatestReportByType,
+} = require('../services/reports');
+const { sendMail } = require('../services/mail');
 
 module.exports = async (logger) => {
   try {
-    await generateReport();
-    logger.info(`Generated and saved report.`);
+    const response = await generateReport();
+
+    const recipient = 'r.sahakyan1@gmail.com'; // need to replace with the emails of the actual recipients - waiting for confirmation
+    const recipientName = 'GYB'; // waiting for confirmation
+
+    if (response.success) {
+      // generated report successfully
+      logger.info(response.message);
+
+      // Fetch the latest report
+      const report = await getLatestReportByType('historic');
+
+      if (report) {
+        const subject = 'New GYB Historic Report Available';
+        const emailHTML =
+          '<p>A new report with historic data (to-date) is available.</p>';
+        // Send the report
+        const mailResponse = await sendMail(
+          subject,
+          emailHTML,
+          recipient,
+          recipientName,
+          report
+        );
+        if (mailResponse.success) {
+          logger.info(mailResponse.message);
+        } else {
+          logger.error(`Failed to send email: ${mailResponse.err}`);
+        }
+      } else {
+        logger.error('No report found');
+      }
+    } else {
+      // Report generation failed
+      logger.error(response.message);
+      const subject = 'Issue with Generating Historic Report';
+      const emailHTML =
+        '<p>There was an issue with generating the historic report.</p>';
+
+      // Send the error email
+      const mailResponse = await sendMail(
+        subject,
+        emailHTML,
+        recipient,
+        recipientName
+      );
+      if (mailResponse.success) {
+        logger.info(mailResponse.message);
+      } else {
+        logger.error(`Failed to send email: ${mailResponse.err}`);
+      }
+    }
   } catch (error) {
     logger.error(`Error generating historic report: ${error}`);
   }
