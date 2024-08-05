5ae18fd5c62cc475ebb27f6319088ffba50a76f7
Haydn Appleby <haydnba@pm.me>
2024-08-06 00:01:28 +0100

    exclude ususal login stuff and restrict to jsut login options

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -79,20 +79,8 @@ export const Login = () => {
 
   return (
     <Container data-id="LoginRoute">
-      <StyledTabs forceRenderTabPanel={true} defaultIndex={1}>
-        <StyledTabList>
-          <SignUpStyledTab>Sign Up</SignUpStyledTab>
-          <StyledTab>Log In</StyledTab>
-          <HiddenStyledTab className="adddonor">Donor signup</HiddenStyledTab>
-          <HiddenStyledTab className="addshopper">
-            Shopper signup
-          </HiddenStyledTab>
-        </StyledTabList>
-
-        <StyledTabPanel>
-          <SignUpContainer />
-        </StyledTabPanel>
-        <StyledTabPanel>
+      {true && (
+        <>
           <ResetPassword
             visible={visible}
             handleOk={handleReset}
@@ -129,14 +117,69 @@ export const Login = () => {
             </StyledForm>
           </Formik>
           {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-        </StyledTabPanel>
-        <StyledTabPanel>
-          <DonorSignUpForm />
-        </StyledTabPanel>
-        <StyledTabPanel>
-          <ShopperSignUpForm />
-        </StyledTabPanel>
-      </StyledTabs>
+        </>
+      )}
+
+      {false && (
+        <StyledTabs forceRenderTabPanel={true} defaultIndex={1}>
+          <StyledTabList>
+            <SignUpStyledTab>Sign Up</SignUpStyledTab>
+            <StyledTab>Log In</StyledTab>
+            <HiddenStyledTab className="adddonor">Donor signup</HiddenStyledTab>
+            <HiddenStyledTab className="addshopper">
+              Shopper signup
+            </HiddenStyledTab>
+          </StyledTabList>
+
+          <StyledTabPanel>
+            <SignUpContainer />
+          </StyledTabPanel>
+          <StyledTabPanel>
+            <ResetPassword
+              visible={visible}
+              handleOk={handleReset}
+              handleCancel={handleCancelReset}
+              loading={loading}
+            />
+            <Formik
+              initialValues={{ email: '', password: '' }}
+              validationSchema={loginSchema}
+              onSubmit={handleLoginSubmit}
+            >
+              <StyledForm>
+                <div>
+                  <StyledLabel>Email address</StyledLabel>
+                  <StyledInput name="email" placeholder="Enter email address" />
+                  <StyledError name="email" component="div" />
+
+                  <StyledLabel>Password</StyledLabel>
+                  <StyledInputPassword
+                    name="password"
+                    placeholder="Enter password"
+                  />
+                  <StyledError name="password" component="div" />
+                  <NewPasswordLink
+                    onClick={() => {
+                      setVisible(true);
+                    }}
+                  >
+                    Request a new password
+                  </NewPasswordLink>
+                </div>
+
+                <StyledSubmitButton>Log In</StyledSubmitButton>
+              </StyledForm>
+            </Formik>
+            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+          </StyledTabPanel>
+          <StyledTabPanel>
+            <DonorSignUpForm />
+          </StyledTabPanel>
+          <StyledTabPanel>
+            <ShopperSignUpForm />
+          </StyledTabPanel>
+        </StyledTabs>
+      )}
     </Container>
   );
 };
