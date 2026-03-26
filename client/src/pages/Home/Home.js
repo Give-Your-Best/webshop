ad7ae22e16c2646941a9c8526d8429a81987d016
Robert Sahakyan <r.sahakyan1@gmail.com>
2026-03-26 17:52:19 +0100

    clean up Home component by removing commented code and unused imports

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,16 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Button, ItemCard } from '../../components';
-// import { CategoriesCarousel } from '../../components/organisms';
 import { getItems } from '../../services/items';
-import {
-  CoverWrapper,
-  ItemsWrapper,
-  // CategoriesWrapper,
-  H1,
-  ButtonWrapper,
-} from './Home.styles';
+import { CoverWrapper, ItemsWrapper, H1, ButtonWrapper } from './Home.styles';
 import logo from './website_cover.png';
-// import { subCategories } from '../../utils/constants';
 import { useHistory } from 'react-router-dom';
 import { getSetting } from '../../services/settings';
 
@@ -50,15 +42,6 @@ export const Home = () => {
         <p>shop up to {shopItemLimit} items per week for free!</p>
         <img alt="give-your-best-cover" src={logo} />
       </CoverWrapper>
-
-      {/* <CategoriesWrapper>
-        <h1>Browse Categories</h1>
-        <CategoriesCarousel
-          categories={subCategories.filter((c) => {
-            return c.parentCategory === 'women' && c.image;
-          })}
-        />
-      </CategoriesWrapper> */}
       <H1>All Items</H1>
       <ItemsWrapper my={1} mx={-1} display="flex" flexWrap="wrap">
         {items.length
